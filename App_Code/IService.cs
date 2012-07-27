60afccedf7f657c1a0ab4dee83e5a68c322a12d0
Israel Romero <israelalkarim@gmail.com>
2012-07-27 03:15:46 -0500

    sandvich

---
+++ App_Code/IService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService" in both code and config file together.
+[ServiceContract]
+public interface IService
+{
+    //Servicio de Usuarios (sino, como se logea, duuuh)
+    [OperationContract]
+    DataSet showUsuarios();
+
+    [OperationContract]
+    DataSet findUsuario(String dni);
+
+    //Servicio de Instrumentos
+    [OperationContract]
+    DataSet showInstrumentos();
+
+    [OperationContract]
+    DataSet findInstrumento(String cod);
+
+    //Servicio de Studios
+    [OperationContract]
+    DataSet showStudios();
+
+    [OperationContract]
+    DataSet findStudio(String cod);
+
+    //Servicio de Reserva
+    [OperationContract]
+    DataSet showReservas();
+
+    [OperationContract]
+    DataSet findReserva(String cod);
+
+}
+
+
---
+++ App_Code/Service.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class Service : IService
+{
+    private SqlConnection cn = new SqlConnection("server=.; database=Comercial2012; uid=sa; pwd=sql");
+
+    System.Data.DataSet IService.showUsuarios()
+    {
+        SqlDataAdapter da = new SqlDataAdapter("Select * from Usuarios", cn);
+        DataSet ds = new DataSet();
+        da.Fill(ds);
+        return ds;
+    }
+
+}
